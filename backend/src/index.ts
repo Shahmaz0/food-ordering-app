7d91ca2b6972744738158cc5c0af650337bfc14d
Shahma Ansari <shahmaansari@Shahmas-MacBook-Air.local>
2024-03-27 04:47:23 +0530

    added user schema model and user endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import * as mongoose from "mongoose";
+import myUserRoutes from "./routes/myUserRoutes";
+import router from "./routes/myUserRoutes";
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
@@ -13,9 +15,7 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.get("/test", async (req: Request, res: Response)=>{
-    res.json("Hello")
-});
+app.use("/api/my/user", myUserRoutes)
 
 app.listen(3000, ()=>{
     console.log("Server started on localhost:3000")
